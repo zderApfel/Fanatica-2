7083b12a4120ec871f9b029d1fb959d1c28ee67b
zderApfel <scoutt172@yahoo.com>
2023-04-21 23:10:14 -0400

    Well THAT was a mess!

--- kubejs/server_scripts/recipe.js
+++ kubejs/server_scripts/recipe.js
@@ -5,38 +5,7 @@ settings.logRemovedRecipes = true
 settings.logSkippedRecipes = false
 settings.logErroringRecipes = true
 
-const shaped = [
-	new ShapedRecipe(
-		'kubejs:silver_sword',
-		'1',
-		'[ S , S , s ,]',
-		{S: 'forge:ingots/silver', s: 'minecraft:stick'}
-	),
-	new ShapedRecipe(
-		'kubejs:silver_pickaxe',
-		'1',
-		'[SSS, s , s ,]',
-		{S: 'forge:ingots/silver', s: 'minecraft:stick'}
-	),
-	new ShapedRecipe(
-		'kubejs:silver_shovel',
-		'1',
-		'[ S , s , s ,]',
-		{S: 'forge:ingots/silver', s: 'minecraft:stick'}
-	),
-	new ShapedRecipe(
-		'kubejs:silver_axe',
-		'1',
-		'[ SS, sS, s ,]',
-		{S: 'forge:ingots/silver', s: 'minecraft:stick'}
-	),
-	new ShapedRecipe(
-		'kubejs:silver_hoe',
-		'1',
-		'[ SS, s , s ,]',
-		{S: 'forge:ingots/silver', s: 'minecraft:stick'}
-	),
-]
+const shaped = []
 
 const shapeless = []
 
